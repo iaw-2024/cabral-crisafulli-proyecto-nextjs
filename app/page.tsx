5276ca6f45349786ceeb3c6d06b0ca629f6170ad
Luz255181 <mira_luz_explo@hotmail.com>
2024-05-24 20:15:18 -0300

    Logo colocado

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <p className="sobre-mi">Sobre mí</p>
+      <p className="sobre-mi">Hola!!!!</p>
       <div className="custom-font">
-        Hola!!!! Mi nombre es Luz y soy la emprendedora detrás de Katty Manualidades. Soy de Bahía Blanca y actualmente no cuento con envíos fuera de Bahía Blanca.
+        Mi nombre es Luz y soy la emprendedora detrás de Katty Manualidades. Soy de Bahía Blanca y actualmente no cuento con envíos fuera de Bahía Blanca.
         El emprendimiento nació en 2019 como una forma de poder costear fotocopias y transporte para la universidad. Actualmente este emprendimiento es mi estilo de vida y mi empleo.
         Katty Manualidades es un emprendimiento principalmente especializado en pulseras de macramé, pero también hacemos muchísimos tipos de accesorios diferentes en variedad de materiales.
         Siempre buscamos la forma de ser originales y ponernos retos personales al momento de crear algo nuevo. Por ello estamos abiertos a recibir consultas acerca de nuestros productos, pedidos mayoristas y a recibir sugerencias de modelos nuevos.
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,8 +6,8 @@ import { signOut } from '@/auth';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <a href="https://imgur.com/usjEbcJ" className="mb-2 flex h-20 items-end justify-start rounded-md bg-purple-400 p-4 md:h-40">
-        <img src="https://imgur.com/usjEbcJ" alt="Katty" className="w-40" />
+      <a className="mb-2 flex h-20 items-end justify-start rounded-md bg-purple-400 p-4 md:h-40">
+        <img src="https://imgur.com/usjEbcJ.png" alt="Katty" className="w-40" />
       </a>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
