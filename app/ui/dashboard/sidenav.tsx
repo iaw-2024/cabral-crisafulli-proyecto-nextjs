74c594fc367e8656c8437e3514ede7cd86c183d9
Luz255181 <mira_luz_explo@hotmail.com>
2024-05-26 14:15:01 -0300

    Agregado Image a sidenav

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,6 +2,7 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
 import { signOut } from '@/auth';
 import { PrismaClient } from '@prisma/client'
+import Image from 'next/image';
 
 export default async function SideNav() {
   const prisma = new PrismaClient()
@@ -13,7 +14,12 @@ export default async function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <a className="mb-2 flex h-20 items-end justify-start rounded-md bg-purple-400 p-4 md:h-40">
-        <img src={logo?.fotoURL} alt="Katty" className="w-40" />
+        <Image src={logo?.fotoURL}
+          alt="Katty"
+          width={283}
+          height={283}
+          className='w-40'
+        />
       </a>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
