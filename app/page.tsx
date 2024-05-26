3017b00caf2a33075691e522319f8eb473464d83
Luz255181 <mira_luz_explo@hotmail.com>
2024-05-26 14:44:39 -0300

    Foto emprendedor agregado sobre mi

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next';
 import React from 'react';
 import '@/app/ui/global.css';
-import { Suspense } from 'react';
+import { PrismaClient } from '@prisma/client';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: {
@@ -10,19 +11,28 @@ export const metadata: Metadata = {
   },
 };
 
-
-export default function Page() {
+export default async function Page() {
+  const prisma = new PrismaClient()
+  const foto = await prisma.fotos.findFirst({
+    where: {
+      nombre: 'Foto_Emprendedor',
+    },
+  })
   return (
     <>
-    <Suspense>
-    <p className="sobre-mi">Hola!!!!</p>
+      <p className="sobre-mi">Hola!!!!</p>
       <div className="custom-font">
         Mi nombre es Luz y soy la emprendedora detrás de Katty Manualidades. Soy de Bahía Blanca y actualmente no cuento con envíos fuera de Bahía Blanca.
         El emprendimiento nació en 2019 como una forma de poder costear fotocopias y transporte para la universidad. Actualmente este emprendimiento es mi estilo de vida y mi empleo.
         Katty Manualidades es un emprendimiento principalmente especializado en pulseras de macramé, pero también hacemos muchísimos tipos de accesorios diferentes en variedad de materiales.
         Siempre buscamos la forma de ser originales y ponernos retos personales al momento de crear algo nuevo. Por ello estamos abiertos a recibir consultas acerca de nuestros productos, pedidos mayoristas y a recibir sugerencias de modelos nuevos.
       </div>
-    </Suspense>
+      <Image src={foto?.fotoURL as string}
+        alt="Emprendedor"
+        width={283}
+        height={283}
+        className='w-50'
+      />
     </>
   );
 }
