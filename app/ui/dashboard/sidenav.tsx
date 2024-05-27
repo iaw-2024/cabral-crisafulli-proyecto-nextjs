f6cb342b39a633a4d3ed25c76db03c5fd809932f
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-05-27 18:21:50 -0300

    Logo centrado y con responsive

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -13,12 +13,13 @@ export default async function SideNav() {
   })
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <a className="mb-2 flex h-20 items-end justify-start rounded-md bg-purple-400 p-4 md:h-40">
-        <Image src={logo?.fotoURL as string}
+      <a className="mb-2 flex h-20 items-center justify-center rounded-md bg-purple-400 p-4 md:h-40">
+        <Image
+          src={logo?.fotoURL as string}
           alt="Katty"
           width={283}
           height={283}
-          className='w-40'
+          className='w-20 h-auto sm:w-24 md:w-32 lg:w-40'
         />
       </a>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
