fc208852d9a9df25d97017b5b387d9bfd9e9bcdd
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-06 17:24:30 -0300

    Boton Vaciar el carrito (no funciona aun)

--- app/dashboard/carrito/page.tsx
+++ app/dashboard/carrito/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 import React from 'react';
 import '@/app/ui/global.css';
+import { ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 export const metadata: Metadata = {
     title: {
@@ -8,12 +9,26 @@ export const metadata: Metadata = {
         default: 'Carrito',
     },
 };
+const CancelledCartIcon = () => {
+    return (
+        <div className="relative inline-block">
+            <ShoppingCartIcon className="h-6 w-6 text-white-500" />
+            <XMarkIcon className="h-4 w-4 text-white-500 absolute top-0 right-0" />
+        </div>
+    );
+};
 
 
 export default function Page() {
     return (
         <>
             <p className="carrito">Carrito</p>
+            <div className="flex justify-end">
+                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex items-center">
+                    <CancelledCartIcon />
+                    <span className="ml-2">Vaciar carrito</span>
+                </button>
+            </div>
         </>
     );
 }
