88053be689aac5853ea08b9023c21efaac367746
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-15 23:01:40 -0300

    deshabilitados los botones si carrito vacio

--- app/dashboard/carrito/page.tsx
+++ app/dashboard/carrito/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 import '@/app/ui/global.css';
-import { ShoppingCartIcon, XMarkIcon, CreditCardIcon } from '@heroicons/react/24/outline';
-import React, { useEffect, useRef } from 'react';
+import { ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks'
 import ProductCartTable from '@/app/ui/cart/table';
 import { vaciarCarrito } from '@/redux/features/carrito/carritoSlice';
 import Link from 'next/link';
-
 
 const CancelledCartIcon = () => {
     return (
@@ -17,24 +16,36 @@ const CancelledCartIcon = () => {
     );
 };
 
-
 export default function Page() {
-
-    //TODO usar los productos del estado para la pagina 
+    // Obtener los productos del estado
     const products = useAppSelector(state => state.productos)
     const dispatch = useAppDispatch()
+
+    // Estado para habilitar o deshabilitar los botones
+    const [isDisabled, setIsDisabled] = useState(false);
+
+    // Efecto para actualizar el estado basado en la cantidad de productos
+    useEffect(() => {
+        setIsDisabled(products.length === 0);
+    }, [products]);
+
     return (
         <>
             <p className="carrito">Carrito</p>
             <div className="flex justify-end">
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex items-center"
-                    onClick={() => { dispatch(vaciarCarrito()) }}>
+                <button
+                    className={`${isDisabled ? 'bg-red-400 cursor-not-allowed' : 'bg-red-500 hover:bg-red-700'
+                        } text-white font-bold py-2 px-4 rounded flex items-center`}
+                    onClick={() => { if (!isDisabled) dispatch(vaciarCarrito()) }}
+                    disabled={isDisabled}>
                     <CancelledCartIcon />
                     <span className="ml-2">Vaciar carrito</span>
                 </button>
                 <Link
-                    href="/dashboard/pagar"
-                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center">
+                    href={isDisabled ? "#" : "/dashboard/pagar"}
+                    onClick={(e) => isDisabled && e.preventDefault()}
+                    className={`${isDisabled ? 'bg-purple-400 cursor-not-allowed' : 'bg-green-500 hover:bg-green-700'
+                        } text-white font-bold py-2 px-4 rounded flex items-center`}>
                     Pagar
                 </Link>
             </div>
