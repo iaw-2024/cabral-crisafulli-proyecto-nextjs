815d5999c724242508dd19503224329a9979207b
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-18 16:32:02 -0300

    modificacion redireccion not-found

--- app/dashboard/productos/[id]/not-found.tsx
+++ app/dashboard/productos/[id]/not-found.tsx
@@ -1,17 +1,35 @@
 import Link from 'next/link';
 import { FaceFrownIcon } from '@heroicons/react/24/outline';
+import { useAppSelector } from '@/redux/hooks';
 
 export default function NotFound() {
-  return (
-    <main className="flex h-screen flex-col items-center justify-center gap-4 p-4 md:p-8">
-      <FaceFrownIcon className="w-10 text-gray-400 md:w-16 text-purple-700" />
-      <h2 className="text-xl font-semibold text-purple-700 md:text-2xl lg:text-3xl">No se encontraron resultados para lo que busca</h2>
-      <Link
-        href="/dashboard/productos"
-        className="mt-4 rounded-md bg-purple-700 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-600 md:px-6 md:py-3 lg:px-8 lg:py-4"
-      >
-        Volver
-      </Link>
-    </main>
-  );
+  const logueado = useAppSelector(state => state.log)
+  if (!logueado) {
+    return (
+      <main className="flex h-screen flex-col items-center justify-center gap-4 p-4 md:p-8">
+        <FaceFrownIcon className="w-10 text-gray-400 md:w-16 text-purple-700" />
+        <h2 className="text-xl font-semibold text-purple-700 md:text-2xl lg:text-3xl">No se encontraron resultados para lo que busca</h2>
+        <Link
+          href="/dashboard/productos"
+          className="mt-4 rounded-md bg-purple-700 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-600 md:px-6 md:py-3 lg:px-8 lg:py-4"
+        >
+          Volver
+        </Link>
+      </main>
+    );
+  } else {
+    return (
+      <main className="flex h-screen flex-col items-center justify-center gap-4 p-4 md:p-8">
+        <FaceFrownIcon className="w-10 text-gray-400 md:w-16 text-purple-700" />
+        <h2 className="text-xl font-semibold text-purple-700 md:text-2xl lg:text-3xl">No se encontraron resultados para lo que busca</h2>
+        <Link
+          href="/dashboard/admin"
+          className="mt-4 rounded-md bg-purple-700 px-4 py-2 text-sm text-white transition-colors hover:bg-purple-600 md:px-6 md:py-3 lg:px-8 lg:py-4"
+        >
+          Volver
+        </Link>
+      </main>
+    );
+  }
+
 }
