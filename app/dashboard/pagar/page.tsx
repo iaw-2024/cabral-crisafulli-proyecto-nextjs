2f6716bbfc8738ec7eba118fc5062bc8c524c9be
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-06-18 10:48:42 -0300

    Compila pero todavia no anda el boton de pagar del formulario

--- app/dashboard/pagar/page.tsx
+++ app/dashboard/pagar/page.tsx
@@ -3,19 +3,19 @@ import PayForm from '@/app/ui/productos/pay-form';
 
 export default async function Page() {
   const formData = new FormData();
-    return (
-      <main>
-        <Breadcrumbs
-          breadcrumbs={[
-            { label: 'Carrito', href: '/dashboard/carrito' },
-            {
-              label: 'Pagar producto',
-              href: '/dashboard/pagar',
-              active: true,
-            },
-          ]}
-        />
-         <PayForm formData={formData}/>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Carrito', href: '/dashboard/carrito' },
+          {
+            label: 'Pagar producto',
+            href: '/dashboard/pagar',
+            active: true,
+          },
+        ]}
+      />
+      <PayForm formData={formData} />
+    </main>
+  );
+}
