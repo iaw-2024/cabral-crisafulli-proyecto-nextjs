e3fe25ae4739de2c48a03c632edef9a35feb1679
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-19 16:58:34 -0300

    persist listo

--- app/storeProvider.tsx
+++ app/storeProvider.tsx
@@ -2,6 +2,8 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from '../redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist'
 
 export default function StoreProvider({
     children
@@ -14,5 +16,11 @@ export default function StoreProvider({
         storeRef.current = makeStore()
     }
 
-    return <Provider store={storeRef.current}>{children}</Provider>
+    let persistor = persistStore(storeRef.current);
+
+    return <Provider store={storeRef.current}>
+        <PersistGate loading={null} persistor={persistor}>
+            {children}
+        </PersistGate>
+    </Provider>
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import carritoSlice from './features/carrito/carritoSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, carritoSlice)
 
 export const makeStore = () => {
   return configureStore({
-    reducer: carritoSlice
+    reducer: persistedReducer
   })
 }
 
