d26643fc25bc570f44a472760c711742469b588e
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-21 18:15:06 -0300

    modificacion WalletButton MP

--- app/ui/cart/walletButton.tsx
+++ app/ui/cart/walletButton.tsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { initMercadoPago } from '@mercadopago/sdk-react';
 import { Wallet } from '@mercadopago/sdk-react';
 
-initMercadoPago(process.env.MP_PUBLIC_KEY!);
+const MP_PUBLIC_KEY = "APP_USR-96d39e1c-68d5-45f1-8879-ed5e6023a20e"
+
+initMercadoPago(MP_PUBLIC_KEY);
 
 export function WalletButton({ preferenceId }: { preferenceId: string }) {
     return (
