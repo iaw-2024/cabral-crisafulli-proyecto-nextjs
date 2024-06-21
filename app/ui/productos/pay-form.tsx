5aa6cb96c0033758c43f967334fac7960d1d850e
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-21 18:18:32 -0300

    Eliminado WalletButton

--- app/ui/productos/pay-form.tsx
+++ app/ui/productos/pay-form.tsx
@@ -3,8 +3,11 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useAppSelector } from '@/redux/hooks';
-import { WalletButton } from '../cart/walletButton';
+import { Wallet, initMercadoPago } from '@mercadopago/sdk-react';
 
+const MP_PUBLIC_KEY = "APP_USR-96d39e1c-68d5-45f1-8879-ed5e6023a20e"
+
+initMercadoPago(MP_PUBLIC_KEY);
 
 const PayForm = () => {
     const productos = useAppSelector(state => state.productos);
@@ -128,7 +131,7 @@ const PayForm = () => {
                     {preferenceId && (
                         <div id="wallet_container">
                             <div className="mt-6">
-                                <WalletButton preferenceId={preferenceId} />
+                                <Wallet initialization={{ preferenceId }} />
                             </div>
                         </div>
                     )}
