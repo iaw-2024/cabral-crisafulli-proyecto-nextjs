5e6bb8a143ff3b4a113d7553a6e29a9d68f12d96
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-06-22 09:14:01 -0300

    Funciona el /admin oficialmente

--- app/ui/admin/redirectLogin.tsx
+++ app/ui/admin/redirectLogin.tsx
@@ -1,17 +1,23 @@
 'use client'
 
+import { useEffect, useRef } from 'react';
 import { useAppSelector } from "@/redux/hooks";
 import { usePathname, useRouter } from "next/navigation";
 
-
 export function RedirectAdmin() {
     const logueado = useAppSelector(state => state.log);
-    const router = useRouter()
+    const router = useRouter();
     const pathname = usePathname();
-    if (pathname === '/dashboard/admin' && !logueado) {
-        router.push('/dashboard/login');
-    }
-    return (
-        <></>
-    )
+    const previousPathname = useRef<string | null>(null);
+
+    useEffect(() => {
+        if (pathname !== previousPathname.current) {
+            if (pathname === '/dashboard/admin' && !logueado) {
+                router.push('/dashboard/login');
+            }
+            previousPathname.current = pathname;
+        }
+    }, [pathname, logueado, router]);
+
+    return null;
 }
