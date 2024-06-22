ca2c30664747ee838a436c1f15e45f2917a964f0
Sofi Crisafulli <71233317+SofiaCrisafulli@users.noreply.github.com>
2024-06-22 16:13:39 -0300

    Cambio de descripcion de sobre mi

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,10 @@ export default async function Page() {
     <>
       <p className="sobre-mi">Hola!!!!</p>
       <div className="custom-font">
-        Mi nombre es Luz y soy la emprendedora detrás de Katty Manualidades. Soy de Bahía Blanca y actualmente no cuento con envíos fuera de Bahía Blanca.
-        El emprendimiento nació en 2019 como una forma de poder costear fotocopias y transporte para la universidad. Actualmente este emprendimiento es mi estilo de vida y mi empleo.
-        Katty Manualidades es un emprendimiento principalmente especializado en pulseras de macramé, pero también hacemos muchísimos tipos de accesorios diferentes en variedad de materiales.
-        Siempre buscamos la forma de ser originales y ponernos retos personales al momento de crear algo nuevo. Por ello estamos abiertos a recibir consultas acerca de nuestros productos, pedidos mayoristas y a recibir sugerencias de modelos nuevos.
+        Mi nombre es Luz y soy la emprendedora detrás de Katty Manualidades. Soy de Bahía Blanca y actualmente no cuento con envíos fuera de esta ciudad.
+        El emprendimiento nació en 2019 como una forma de poder costear fotocopias y transporte para la universidad. Actualmente este es mi estilo de vida y mi empleo.
+        Katty Manualidades está especializado en pulseras de macramé, pero también hacemos muchísimos tipos de accesorios diferentes en variedad de materiales.
+        Siempre buscamos la forma de ser originales y ponernos retos personales al momento de crear algo nuevo. Por ello, estamos abiertos a recibir consultas acerca de nuestros productos, pedidos mayoristas y a recibir sugerencias de modelos nuevos.
       </div>
       <div className="flex justify-center mt-4">
         <Image src={(await foto)?.fotoURL as string}
