b7a286ff858888142a5d044456505ad871987689
Luz255181 <mira_luz_explo@hotmail.com>
2024-06-29 13:45:11 -0300

    modificacion de carrito

--- app/ui/cart/table.tsx
+++ app/ui/cart/table.tsx
@@ -11,29 +11,29 @@ export default function ProductCartTable({
     productos: Product[]
 }) {
     const dispatch = useAppDispatch();
-
     return (
         <div className="bg-white px-4 sm:px-6 lg:px-8">
             {productos.map((product: Product) => (
                 <div key={product.id} className="border border-gray-400 rounded-lg mb-4">
-                    <div className="grid grid-cols-1 md:grid-cols-2">
+                    <div>
                         <div className="p-4 flex justify-center items-center">
                             <Image
                                 key={product.id}
                                 src={product.fotoURL}
                                 alt={`${product.nombre}`}
-                                className="w-48 h-48 md:w-64 md:h-64 object-contain"
+                                className="w-64 h-64 object-contain"
                                 width={160}
                                 height={160}
                             />
                         </div>
                         <div className='p-4'>
                             <p className="text-lg font-bold mb-2">{product.nombre}</p>
-                            <p className="text-gray-600 mb-2">{product.precio.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</p>
+                            <p className="text-gray-600 mb-2">{product.precio.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })} x {product.quantity}</p>
+                            <p className="text-gray-900 mb-2">{(product.precio * product.quantity).toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}</p>
                             <button
                                 className="btn-borrar-carrito bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-2 rounded flex items-center justify-center"
                                 onClick={() => dispatch(borrarDeCarrito(product))}
-                              
+
                             >
                                 <TrashIcon className="h-5 w-5" />
                             </button>
